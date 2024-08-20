25d3932308dc993ed346728c07e31dfc63bc011e
siddharth2440 <siddharth2440.live@gmail.com>
2024-08-21 00:01:56 +0530

    homePage UI in progress

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Post from './Post'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div className='my-3 w-[100%] flex flex-col items-start justify-start gap-7'>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </div>
   )
 }
 
